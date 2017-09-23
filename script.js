6abd42dfc248f0e095eb8498ad2234443a516017
AntonioContrerasRomán <antoniocontrerasroman@gmail.com>
2017-09-23 21:27:16 +0200

    changes in css and js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     $('.hamburguer').click(function(event) {
         event.preventDefault();
-        $('.menuResponsive').css({transform: "translateX(0px)"});
+        $('menuResponsive')
     });
 });
